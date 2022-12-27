93a5a5ebd06948c43048adf4f3b75e5e8092af56
gytjd <hys339631@gmail.com>
2022-12-27 15:57:52 +0900

    프로젝트 생성

---
+++ src/main/java/GDSC/Backend4th/Backend4thApplication.java
@@ -0,0 +1,13 @@
+package GDSC.Backend4th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend4thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend4thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSC/Backend4th/Backend4thApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSC.Backend4th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend4thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
