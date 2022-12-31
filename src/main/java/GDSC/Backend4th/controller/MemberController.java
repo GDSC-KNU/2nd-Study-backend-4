686a8723b08deb9c66bb38e7cd557eb34b1c78b8
im2sh <104557685+im2sh@users.noreply.github.com>
2022-12-31 22:10:54 +0900

    feat : 회원가입 페이지 추가

---
+++ src/main/java/GDSC/Backend4th/controller/MemberController.java
@@ -0,0 +1,31 @@
+package GDSC.Backend4th.controller;
+
+import GDSC.Backend4th.domain.member.Member;
+import GDSC.Backend4th.service.member.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/join")
+    public String joinForm(@ModelAttribute Member member){
+        return "member/joinMemberForm";
+    }
+
+    @PostMapping("/join")
+    public String saveMember(@Valid @ModelAttribute Member member, BindingResult result){
+        if(result.hasErrors()){
+            return "member/joinMemberForm";
+        }
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
